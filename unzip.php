a5dc343e123af7a1b222b02d273d883c30ecd9dc
Hamza Iqbal <hamza.hamza554@gmail.com>
2019-02-10 03:01:33 +0500

    Update unzip.php

--- unzip.php
+++ unzip.php
@@ -1,1 +1,17 @@
 
+<?php
+// File name
+$file = 'file.zip';
+
+$path = pathinfo(realpath($file), PATHINFO_DIRNAME);
+
+$zip = new ZipArchive;
+$res = $zip->open($file);
+if ($res === TRUE) {
+  $zip->extractTo($path);
+  $zip->close();
+  echo "WOOT! $file extracted to $path";
+} else {
+  echo "Doh! I couldn't open $file";
+}
+?>
